adef8d8892e474f0a16b182c86707158caa60e6d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-14 19:09:00 -0300

    (style) Configurados import aliases

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { SendNotification } from 'src/application/use-cases/send-notification';
-import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { CreateNotificationBody } from '@infra/http/dtos/create-notification-body';
 
 @Controller()
 export class NotificationsController {
