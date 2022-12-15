cacb0951b52742b32eb3b3d260be561528024d06
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-14 22:17:20 -0300

    (feat) métodos implementados no repositório de notificações prisma

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -6,16 +6,50 @@ import { PrismaNotificationMapper } from '@infra/mappers/prisma-notification-map
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prisma.notification.count({
+      where: {
+        recipientId: recipientId,
+      },
+    });
+
+    return count;
+  }
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prisma.notification.findMany({
+      where: {
+        recipientId: recipientId,
+      },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
   async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+    const notification = await this.prisma.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if (!notification) {
+      return null;
+    }
+
+    return PrismaNotificationMapper.toDomain(notification);
   }
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+    await this.prisma.notification.update({
+      data: raw,
+      where: {
+        id: raw.id,
+      },
+    });
   }
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
-    await this.prismaService.notification.create({
+    await this.prisma.notification.create({
       data: raw,
     });
   }
